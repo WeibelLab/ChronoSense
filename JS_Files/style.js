8d5b62e653cefcf8a054795291999a444a75ac72
Alexander Schonken <ajschonken@gmail.com>
2020-04-09 18:03:42 -0700

    Buttons now swap between features

--- JS_Files/style.js
+++ JS_Files/style.js
@@ -3,12 +3,22 @@
 // All of the Node.js APIs are available in this process.
 const remote = require('electron').remote;
 
+
+const viewerWindow = document.getElementById('content-selection');
+const btnHome = document.getElementById('homePage');
+const btnWebcam = document.getElementById('webcamPage');
+const btnKinect = document.getElementById('kinectPage');
+const btnKinectBodyTracking = document.getElementById('kinectBodyPage');
+const btnAbout = document.getElementById('aboutPage');
+
+
 // When document has loaded, initialise
 document.onreadystatechange = () => {
     if (document.readyState == "complete") {
         handleWindowControls();
     }
 };
+
 
 function handleWindowControls() {
 
@@ -43,5 +53,37 @@ function handleWindowControls() {
             document.body.classList.remove('maximized');
         }
     }
-}
 
+    /*
+    * Set of functions below are for clicking buttons <a> elements on the main
+    * navigation bar for the application. Mainly to switch src of webview to 
+    * change the contents of the page.
+    * 
+    * Buttons: homePage, webcamPage, kinectPage, kinectBodyPage, aboutPage
+    */
+    btnHome.addEventListener("click", event => {
+        console.log("YOU ARE HOME");
+        //Add HTML for Home page as source for the web viewer
+    });
+
+    btnWebcam.addEventListener("click", event => {
+        console.log("YOU ARE WEBCAM");
+        viewerWindow.src = "../HTML_Files/webcam.html";
+    });
+
+    btnKinect.addEventListener("click", event => {
+        console.log("YOU ARE KINECT CAM");
+        viewerWindow.src = "../HTML_Files/kinect.html";
+    });
+
+    btnKinectBodyTracking.addEventListener("click", event => {
+        console.log("YOU ARE BODY TRACKING");
+        viewerWindow.src = "../HTML_Files/kinect_body.html";
+    });
+
+    btnAbout.addEventListener("click", event => {
+        console.log("YOU ARE ABOUT");
+        //Add HTML for an about page to the src of webviewer
+    });
+
+} //End of handleWindowControls()
