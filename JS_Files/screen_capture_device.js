cb5f26ee9eefc396a700f07cdbfac70161b3c313
Alexander Schonken <ajschonken@gmail.com>
2021-02-03 16:54:25 -0800

    Preview of screen options implemented - needs activation function

--- JS_Files/screen_capture_device.js
+++ JS_Files/screen_capture_device.js
@@ -4,7 +4,11 @@ export class ScreenCaptureDevice {
 
     #label = "Screen Capture";
     #deviceId = "ScreenCaptureModule";
-    #sources = []
+	#videoElement = null;
+	#videoContainer = null;
+	#optionContainer = null;
+	#sources = []
+	
 
     constructor() {
         this.getCaptureSources();
@@ -15,6 +19,57 @@ export class ScreenCaptureDevice {
             this.#sources = sources;
             console.log(sources);
         });
+	}
+
+	displaySourceOptions() {
+		// Display source options on top of video element with thumbnail & name.
+		// User can click on one to select it and start streaming in video/audio.
+		
+		// Refresh sources
+		this.getCaptureSources();
+		// Display sources over video
+		this.#sources.forEach(source => {
+			// <div><img/><p>name_of_source</p></div>
+			let newDiv = document.createElement("div");
+			let newImage = document.createElement("img");
+			let newP = document.createElement("p");
+
+			newDiv.style.width = "30%";
+			newDiv.style.height = "auto";
+			newDiv.style.margin = "20px";
+			newDiv.style.borderWidth = "1px";
+			newDiv.style.borderColor = "rgba(255, 255, 255, 0)";
+			newDiv.style.borderRadius = "2px";
+			newDiv.style.cursor = "pointer";
+			newDiv.style.overflow = "hidden";
+
+			newDiv.onmouseover = () => {
+				newDiv.style.borderStyle = "solid";
+				newDiv.style.borderColor = "white";
+
+			};
+
+			newDiv.onmouseout = () => {
+				newDiv.style.borderColor = "rgba(255, 255, 255, 0)";
+
+			};
+
+			newImage.src = source.thumbnail.toDataURL();
+			newImage.style.width = "100%";
+			newImage.style.height = "auto";
+
+			newP.appendChild(document.createTextNode(source.name));
+			newP.style.width = "100%";
+
+			newDiv.appendChild(newImage);
+			newDiv.appendChild(newP);
+
+			this.#optionContainer.appendChild(newDiv);
+
+		});
+
+		// Finally, reveal options
+		this.#optionContainer.style.display = "flex";
 	}
 
 	async startCaptureStream(videoElement) {
@@ -62,20 +117,36 @@ export class ScreenCaptureDevice {
 		//First create the necessary elements
 		// * video, canvas, buttons, video option menus, etc.
 		let videoContainer = document.createElement("div");
+		videoContainer.style.position = "relative";
+		this.#videoContainer = videoContainer;
 		let videoButtonsContainer = document.createElement("div");
 		let videoElement = document.createElement("video");
+		this.#videoElement = videoElement;
 		let canvasElement = document.createElement("canvas");
 		let recordElement = document.createElement("button");
 		let videoButtonsContainerOnOff = document.createElement("div");
 		let onElement = document.createElement("button");
 		let offElement = document.createElement("button");
 
+		// Unique to screen capture to display different capture options.
+		let optionContainer = document.createElement("div");
+		this.#optionContainer = optionContainer;
+		optionContainer.classList.add("camera-canvas"); //! Change later OR allow user to manipulate sizing of containers (e.g. drag edges)
+		optionContainer.style.height = "100%";
+		optionContainer.style.display = "none";
+		optionContainer.style.flexDirection = "row";
+		optionContainer.style.flexWrap = "wrap";
+		optionContainer.style.position = "absolute";
+		optionContainer.style.zIndex = "1";
+		optionContainer.style.overflow = "auto";
+
 		//Set correct properties
 		//Video element is NOT inserted into DOM since it is used as translation to canvas
 		videoElement.width = "1280";
 		videoElement.height = "720";
         videoElement.autoplay = true;
-		videoElement.classList.add("camera-canvas"); //! Change later OR allow user to manipulate sizing of containers
+		videoElement.classList.add("camera-canvas"); //! Change later OR allow user to manipulate sizing of containers (e.g. drag edges)
+		//videoContainerDiv.appendChild(videoElement);
 		
 		//canvasElement.width = "1280";
 		//canvasElement.height = "720";
@@ -91,7 +162,7 @@ export class ScreenCaptureDevice {
 
 		onElement.innerText = "ON";
 		onElement.onclick = () => {
-			this.startCaptureStream(videoElement);
+			this.start();
 		};
 		onElement.classList.add("kinect_on");
 		onElement.style.marginRight = "4px";
@@ -115,6 +186,7 @@ export class ScreenCaptureDevice {
 		videoContainer.id = `${this.getDeviceId()}`;
 
 		//videoContainer.appendChild(canvasElement);
+		videoContainer.appendChild(optionContainer);
         videoContainer.appendChild(videoElement);
         videoContainer.appendChild(videoButtonsContainer);
 
@@ -137,6 +209,15 @@ export class ScreenCaptureDevice {
 	 */
 	getDeviceId() {
 		return this.#deviceId;
+	}
+
+	/**
+	 * Function used to start transmitting data from the device.
+	 */
+	start() {
+		this.displaySourceOptions();
+		//this.startCaptureStream(this.#videoElement);
+
 	}
 
 	/**
