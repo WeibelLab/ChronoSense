497505b206cc3e5b05fa4b934103170f21322ce4
Richard Duong <riduong@ucsd.edu>
2022-04-08 14:17:48 -0700

    Prompt enabling message for first time denial on permissions

--- JS_Files/macOS.js
+++ JS_Files/macOS.js
@@ -2,11 +2,17 @@ const { systemPreferences } = require('electron').remote
 
 export async function fixPermissions(){
     if(process.platform === "darwin") {
-        if(systemPreferences.getMediaAccessStatus("camera") != "granted"){
+        if(systemPreferences.getMediaAccessStatus("camera") == "not-determined"){
             await systemPreferences.askForMediaAccess("camera");
+                if (systemPreferences.getMediaAccessStatus("camera") == 'denied') {
+                    console.log('To enable Camera permission in the future go to System Preferences and allow Camera Access to the ChronoSense App')
+                }
         }
-        if(systemPreferences.getMediaAccessStatus("microphone") != "granted"){
+        if(systemPreferences.getMediaAccessStatus("microphone") == "not-determined"){
             await systemPreferences.askForMediaAccess("microphone");
+                if (systemPreferences.getMediaAccessStatus("microphone") == 'denied') {
+                    console.log('To enable Microphone permission in the future go to System Preferences and allow Microphone Access to the ChronoSense App')
+                }
         }
     }
 }
